cd6ed6c2acb35345e82b4f60083622a524c866f7
Blyiat <tonasaanak@gmail.com>
2024-12-10 09:17:24 +0800

    adding

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<script>
+    export default {
+    data() {
+        return {
+        isMenuOpen: false,
+        isDarkMode: true
+        };
+    },
+    methods: {
+        toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+        },
+        toggleDarkMode() {
+          this.isDarkMode = !this.isDarkMode;
+          if (this.isDarkMode) {
+            document.documentElement.classList.add('dark');
+          } else {
+            document.documentElement.classList.remove('dark');
+          }
+        }
+    }
+    };
+</script>
+<template>
+    <nav class="max-w-[1440px] mx-28 pt-5">
+      <div class="hidden lg:flex text-white justify-between items-center flex">
+        <a href="#" class="hover:bg-white hover:bg-opacity-10 py-2 px-3 duration-300 rounded-xl">Beranda</a>
+        <a href="#" class="hover:bg-white hover:bg-opacity-10 py-2 px-3 duration-300 rounded-xl">Profil</a>
+        <a href="#" class="hover:bg-white hover:bg-opacity-10 py-2 px-3 duration-300 rounded-xl">Alumni</a>
+        <img class="w-16" src="/src/assets/images/pplg-logo.png" alt="">
+        <a href="#" class="hover:bg-white hover:bg-opacity-10 py-2 px-3 duration-300 rounded-xl">Kerja Sama</a>
+        <a href="#" class="hover:bg-white hover:bg-opacity-10 py-2 px-3 duration-300 rounded-xl">Galeri</a>
+        <a href="#" class="hover:bg-white hover:bg-opacity-10 py-2 px-3 duration-300 rounded-xl">Kontak</a>
+      </div>
+
+      <!-- Mobile Menu Icon -->
+      <div class="lg:hidden flex justify-between">
+        <img class="w-16" src="/src/assets/images/pplg-logo.png" alt="">
+          <button class="text-white" @click="toggleMenu">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+      </div>
+
+      <div v-if="isMenuOpen" class="lg:hidden bg-white bg-opacity-80 backdrop-blur-md shadow-2xl shadow-slate-950 border-t border-gray-700">
+        <div class="flex flex-col items-center gap-4 py-5 text-gray-400">
+          <a href="#" class="font-semibold tracking-wide hover:text-gray-300 duration-300">Beranda</a>
+          <a href="#" class="font-semibold tracking-wide hover:text-gray-300 duration-300">Profil Jurusan</a>
+          <a href="#" class="font-semibold tracking-wide hover:text-gray-300 duration-300">Alumni</a>
+          <a href="#" class="font-semibold tracking-wide hover:text-gray-300 duration-300">Kerja Sama</a>
+          <a href="#" class="font-semibold tracking-wide hover:text-gray-300 duration-300">Galeri</a>
+          <a href="#" class="font-semibold tracking-wide hover:text-gray-300 duration-300">Kontak</a>
+        </div>
+      </div>
+    </nav>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<script setup>
+import NavbarVue from '@/components/Navbar.vue';
+
+</script>
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>
+
+<template>
+  <main>
+
+    <div class="relative overflow-hidden z-50">
+      
+      <div class="w-[65rem] h-[65rem] max-2xl:w-[55rem] bg-white rounded-full absolute -left-[30rem] opacity-10 -z-50"></div>
+      <div class="w-[75rem] h-[75rem] bg-white rounded-full absolute -top-[5rem] -left-[30rem] opacity-10 -z-50"></div>
+
+      <div class="w-[55rem] h-[55rem] border border-white rounded-full absolute top-20 -right-[30rem] opacity-25 -z-50"></div>
+      <div class="w-[65rem] h-[65rem] border border-white rounded-full absolute -right-[30rem] opacity-25 -z-50"></div>
+      <div class="w-[75rem] h-[75rem] border border-white rounded-full absolute -top-[5rem] -right-[30rem] opacity-25 -z-50"></div>
+
+      <NavbarVue />
+
+      <section class="max-w-[1440px] flex px-28 z-50 h-[42rem] mt-10">
+        <div class="max-w-2xl space-y-5 pt-24">
+          <h1 class="text-6xl text-white font-semibold tracking-wide leading-tight">Pengembangan Perangkat Lunak dan Game</h1>
+          <h3 class="text-base text-white tracking-wide">Jurusan IT Kekinian Menjadi Ahli Teknologi Masa Depan Pembelajaran yang berfokus pada tren teknologi terkini dan praktik industri</h3>
+          <div class="flex pt-10 gap-10">
+            <a href="#" class="flex w-[270px] justify-between bg-yellow-400 py-2 px-5 rounded-lg tracking-wide font-medium">Jelajahi Jurusan PPLG <svg class="w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 9"><path fill="currentColor" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5"/><path fill="currentColor" d="M10 8.5a.47.47 0 0 1-.35-.15c-.2-.2-.2-.51 0-.71l3.15-3.15l-3.15-3.15c-.2-.2-.2-.51 0-.71s.51-.2.71 0l3.5 3.5c.2.2.2.51 0 .71l-3.5 3.5c-.1.1-.23.15-.35.15Z"/></svg></a>
+            <a href="#" class="flex items-center gap-3 text-white tracking-wide"><svg class="w-10 bg-yellow-400 p-2 rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path class="fill-black" d="M21.409 9.353a2.998 2.998 0 0 1 0 5.294L8.597 21.614C6.534 22.737 4 21.277 4 18.968V5.033c0-2.31 2.534-3.769 4.597-2.648z"/></svg> Yuk Intip Aktivitas Jurusan</a>
+          </div>
+        </div>
+        <div class="flex">
+          <img class="w-[40rem] absolute object-cover" src="/src/assets/images/boy.png" alt="">
+          <img class="w-72 absolute object-cover" src="/src/assets/images/girl.png" alt="">
+        </div>
+        <!-- <div class="flex pt-20">
+          <div class="max-w-fit p-5 bg-[#9167E2] rounded-xl h-fit">
+            <img class="rounded-lg w-96" src="/src/assets/images/pict-1.jpg" alt="">
+          </div>
+        </div> -->
+      </section>
+
+      <section class="h-screen bg-slate-50 rounded-tr-[15rem] rounded-tl-[15rem] py-20">
+        <div class="max-w-[1440px] relative mx-auto space-y-10">
+          <div class="absolute right-20 w-20 h-20 bg-[#7743DB] rounded-full opacity-10"></div>
+          <div class="absolute top-20 left-0 w-20 h-20 border-[10px] border-[#7743DB] rounded-full opacity-10"></div>
+          <div class="absolute bottom-0 right-0 w-16 h-16 bg-[#FFC53B] rounded-full opacity-10"></div>
+          <h2 class="max-w-6xl mx-auto text-center font-semibold text-6xl text-slate-600 leading-tight">Jurusan PPLG fokus pada pembelajaran tentang bagaimana membuat, mengembangkan, dan mengelola Software Komputer</h2>
+          <a href="#" class="flex mx-auto w-[210px] justify-between bg-[#7743DB] text-white py-2 px-5 rounded-lg tracking-wide font-medium">Lebih Lengkap <svg class="w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 9"><path fill="currentColor" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5"/><path fill="currentColor" d="M10 8.5a.47.47 0 0 1-.35-.15c-.2-.2-.2-.51 0-.71l3.15-3.15l-3.15-3.15c-.2-.2-.2-.51 0-.71s.51-.2.71 0l3.5 3.5c.2.2.2.51 0 .71l-3.5 3.5c-.1.1-.23.15-.35.15Z"/></svg></a>
+        </div>
+      </section>
+
+    </div>
+  </main>
+</template>
